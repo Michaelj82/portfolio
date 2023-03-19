ec2da722d2eec3a3a15d607b345c1ac5373477fc
Michael <mjj.yoyogi@gmail.com>
2023-03-18 23:25:50 -0400

    soft scrolling added.

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,62 +1,119 @@
 import React from 'react';
 import {useState, useEffect} from 'react'
+import {Link, animateScroll as scroll} from 'react-scroll'
 import './style.css'
 
 export default function Homepage (){
-    const [typing, setTyping] = useState('')
     
 
     return(
         <div id={'site'}>
-            <header id={'header'}>bruh bruh bruh 
-                bruh bruh bruh</header>
+            <header id={'header'}>
+                <div className='link'>
+                <Link
+                    activeClass='active'
+                    to='intro'
+                    spy={true}
+                    smooth={true}
+                    offset={-30}
+                    duration={200}
+
+                    >Home</Link>
+                </div>
+                <div className='link'>
+                <Link
+                    activeClass='active'
+                    to='description'
+                    spy={true}
+                    smooth={true}
+                    offset={-30}
+                    duration={200}
+
+                    >About Me!</Link>
+                </div>
+                <div className='link'>
+                <Link
+                    activeClass='active'
+                    to='skills'
+                    spy={true}
+                    smooth={true}
+                    offset={-30}
+                    duration={200}
+
+                    >Skills</Link> 
+                </div>
+                <div className='link'>
+                <Link
+                    activeClass='active'
+                    to='projects'
+                    spy={true}
+                    smooth={true}
+                    offset={-30}
+                    duration={200}
+
+                    >Projects</Link> 
+                </div>
+                <div className='link'>
+                <Link
+                    activeClass='active'
+                    to='contactme'
+                    spy={true}
+                    smooth={true}
+                    offset={-30}
+                    duration={200}
+
+                    >Contact Me!</Link>
+                </div>
+
+
+            </header>
             <div id='intro'>
-            yo
+            intro
 
 
             </div>
 
             <div id='description'>
-
+            about me
             </div>
 
             <div id='skills'>
-
+            skills
             </div>
 
 
             <div id='projects'>
 
                 <div className={'element'}>
-                    asdf
+                    project1
                 </div>
                 
                 <div className={'element'}>
-                    asdf
+                project2
 
                 </div>
                 <div className={'element'}>
-                    asdf
+                project3
                     </div>
                 <div className={'element'}>
-                    adf
+                project4
                 </div>
                 
                 <div className={'element'}>
-                    asdf
+                project5
 
                 </div>
                 <div className={'element'}>
-                    asdf
+                project6
                     </div>
             </div>
 
         <div id='contactme'>
-
+        contact me
         </div>
 
         <footer id='footer'>
-            
+            footer
         </footer>
         
         </div>
