824de258addb38d2debbff7338e2687acddcecd4
Michael <mjj.yoyogi@gmail.com>
2023-03-22 15:18:30 -0400

    added a little, plan to do more later while bird ML is running.

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from './Navbar';
 import {useState, useEffect} from 'react'
 import {Link, animateScroll as scroll} from 'react-scroll'
 import './style.css'
@@ -8,65 +9,7 @@ export default function Homepage (){
 
     return(
         <div id={'site'}>
-            <header id={'header'}>
-                <div className='link'>
-                <Link
-                    activeClass='active'
-                    to='intro'
-                    spy={true}
-                    smooth={true}
-                    offset={-30}
-                    duration={200}
-
-                    >Home</Link>
-                </div>
-                <div className='link'>
-                <Link
-                    activeClass='active'
-                    to='description'
-                    spy={true}
-                    smooth={true}
-                    offset={-30}
-                    duration={200}
-
-                    >About Me!</Link>
-                </div>
-                <div className='link'>
-                <Link
-                    activeClass='active'
-                    to='skills'
-                    spy={true}
-                    smooth={true}
-                    offset={-30}
-                    duration={200}
-
-                    >Skills</Link> 
-                </div>
-                <div className='link'>
-                <Link
-                    activeClass='active'
-                    to='projects'
-                    spy={true}
-                    smooth={true}
-                    offset={-30}
-                    duration={200}
-
-                    >Projects</Link> 
-                </div>
-                <div className='link'>
-                <Link
-                    activeClass='active'
-                    to='contactme'
-                    spy={true}
-                    smooth={true}
-                    offset={-30}
-                    duration={200}
-
-                    >Contact Me!</Link>
-                </div>
-
-
-            </header>
+            <Navbar></Navbar>
             <div id='intro'>
             intro
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,66 @@
 import React from 'react'
-
+import { Link } from 'react-scroll'
 export default function Navbar(){
 
-    // return(
-        
-    // )
+    return(
+        <header id={'header'}>
+                <div className='link'>
+                <Link
+                    activeClass='active'
+                    to='intro'
+                    spy={true}
+                    smooth={true}
+                    offset={-30}
+                    duration={200}
+
+                    >Home</Link>
+                </div>
+                <div className='link'>
+                <Link
+                    activeClass='active'
+                    to='description'
+                    spy={true}
+                    smooth={true}
+                    offset={-30}
+                    duration={200}
+
+                    >About Me!</Link>
+                </div>
+                <div className='link'>
+                <Link
+                    activeClass='active'
+                    to='skills'
+                    spy={true}
+                    smooth={true}
+                    offset={-30}
+                    duration={200}
+
+                    >Skills</Link> 
+                </div>
+                <div className='link'>
+                <Link
+                    activeClass='active'
+                    to='projects'
+                    spy={true}
+                    smooth={true}
+                    offset={-30}
+                    duration={200}
+
+                    >Projects</Link> 
+                </div>
+                <div className='link'>
+                <Link
+                    activeClass='active'
+                    to='contactme'
+                    spy={true}
+                    smooth={true}
+                    offset={-30}
+                    duration={200}
+
+                    >Contact Me!</Link>
+                </div>
+
+
+            </header>
+    )
 }
